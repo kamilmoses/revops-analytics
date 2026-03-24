7e82ba94d41c90f9b36b1a19ea4daead4bf9d2b6
Kamil <kamil.mojzesz@gmail.com>
2026-03-24 13:38:59 +0100

    add comments to mart_arr_movement mart

--- models/marts/mart_arr_movement.sql
+++ models/marts/mart_arr_movement.sql
@@ -1,3 +1,4 @@
+-- group ARR by month
 WITH monthly_arr AS (
     SELECT
         CAST(DATE_TRUNC('month',close_date) AS DATE) as month,
@@ -7,14 +8,14 @@ WITH monthly_arr AS (
     GROUP BY DATE_TRUNC('month', close_date)
     ORDER BY month asc
 ),
-
+-- add new column 'prev_month_arr' to compare ARR to previous month
 prev_monthly_arr AS (
     SELECT
         *,
         LAG(monthly_arr) OVER (ORDER BY month) as prev_month_arr
     FROM monthly_arr
 ),
-
+-- add new column 'delta_monthly_arr' to show delta between months (preformance month-to-month)
 delta_monthly_arr AS (
     SELECT 
         *,
