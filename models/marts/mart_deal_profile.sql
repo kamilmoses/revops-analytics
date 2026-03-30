c6aace288f98ab99db2f6e217c57013830fc7eb0
Kamil <kamil.mojzesz@gmail.com>
2026-03-31 00:36:54 +0200

    add LEAD, COALESCE models, fix fan-out in mart_deal_profile

--- models/marts/mart_deal_profile.sql
+++ models/marts/mart_deal_profile.sql
@@ -17,7 +17,16 @@ deals_with_decision_makers AS (
             c.is_decision_maker = TRUE THEN c.contact_name ELSE NULL END AS decision_maker
     FROM all_deals l
     LEFT JOIN {{  ref('stg_contacts')  }} c ON l.account_id = c.account_id
+),
+
+deals_with_one_decision_maker AS (
+    SELECT 
+        *,
+        -- create a flag to see duplicated lines (multiple contacts in contact table)
+        ROW_NUMBER() OVER (PARTITION BY deal_id ORDER BY decision_maker) AS deal_duplicate_numer
+    FROM deals_with_decision_makers
 )
+
 
 SELECT 
     deal_id,
@@ -32,4 +41,6 @@ SELECT
     country,
     industry,
     decision_maker
-FROM deals_with_decision_makers
+FROM deals_with_one_decision_maker
+WHERE deal_duplicate_numer = 1
+ORDER BY close_date ASC 
